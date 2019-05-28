8d116c110d3544d4252a8f27a114c23ccaabda3a
Abdhi <rizaabdhi@gmail.com>
2019-05-29 04:20:18 +0700

    Perbarui Layout

--- app/src/main/java/com/mriksani/dashboarditc/MainActivity.java
+++ app/src/main/java/com/mriksani/dashboarditc/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
